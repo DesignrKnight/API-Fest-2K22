10c4cfad18bb4da4195ad761061a5c533e750d43
Aman Kumar <amankumar141099@gmail.com>
2022-01-28 16:20:31 +0530

    created schwagx server skeleton

---
+++ schwagx-server/src/components/controllers/swags.js
@@ -0,0 +1,21 @@
+const Swag = require('../models/Swag')
+
+exports.getSwags = async (req, res) => {
+
+}
+
+exports.createSwag = async (req, res) => {
+
+}
+
+exports.getSwag = async (req, res) => {
+
+}
+
+exports.updateSwag = async (req, res) => {
+
+}
+
+exports.deleteSwag = async (req, res) => {
+
+}
---
+++ schwagx-server/src/components/models/Swag.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const swagSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+})
+
+const Swag = mongoose.model('Swag', swagSchema)
+
+module.exports = Swag
